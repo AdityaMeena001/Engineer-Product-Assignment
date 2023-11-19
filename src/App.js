c63f84418bdb44f6fc5f7ad9a392c962e4c19024
AdityaMeena001 <adityameena1one@gmail.com>
2023-11-19 23:44:03 +0530

    Added Valo sample comic

--- src/App.js
+++ src/App.js
@@ -1,121 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Components/Card";
-// import ComicBook from "./Components/ComicBook";
 import "./App.css";
 
 const cardContent = [
   {
     id: 1,
-    prompt: "A mysterious comet approaches Earth, emitting strange energy.",
-    enter_text: "Title: The Arrival",
+    enter_text: "Shots coming from distance...",
     leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
+    flx: 400,
+    speech_bubble: "Finally about time you showed up..",
   },
-  {
-    id: 2,
-    prompt:
-      "Scientists discover the comet's energy grants superhuman abilities.",
-    enter_text: "New Characters: Dr. Smith, Dr. Rodriguez",
-    leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
-  },
-  {
-    id: 3,
-    prompt:
-      "Villains and heroes emerge, harnessing the comet's power for good and evil.",
-    enter_text: "Introduction of Villain: Dark Matter",
-    leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
-  },
-  {
-    id: 4,
-    prompt: "A team of unlikely individuals gains extraordinary powers.",
-    enter_text: "Heroic Transformation: Team Formation",
-    leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
-  },
-  {
-    id: 5,
-    prompt:
-      "Tensions rise between those using the energy for personal gain and those protecting humanity.",
-    enter_text: "Conflict Arises: Tension Escalates",
-    leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
-  },
-  {
-    id: 6,
-    prompt:
-      "A decisive battle erupts between the opposing sides, threatening the world's safety.",
-    enter_text: "Epic Battle: Chaos Unleashed",
-    leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
-  },
-  {
-    id: 7,
-    prompt:
-      "The heroes unite, showcasing the strength of unity against the forces of chaos.",
-    enter_text: "Unity Prevails: Heroes Stand Together",
-    leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
-  },
-  {
-    id: 8,
-    prompt:
-      "The villains' defeat restores peace, but remnants of the comet's energy linger, hinting at new challenges.",
-    enter_text: "Victory Achieved: Peace Restored",
-    leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
-  },
-  {
-    id: 9,
-    prompt:
-      "New heroes emerge, ready to defend Earth and explore the unknown powers of the comet.",
-    enter_text: "Introduction of New Heroes: The Next Generation",
-    leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
-  },
-  {
-    id: 10,
-    prompt:
-      "The story continues as the world adapts to the newfound abilities and threats.",
-    enter_text: "New Adventures Begin: Future Awaits",
-    leave_text: "",
-    speech_bubble: "",
-    bold_text: "",
-  },
+  { id: 2, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 3, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 4, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 5, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 6, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 7, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 8, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 9, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 10, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 11, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 12, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 13, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 14, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 15, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 16, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 17, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 18, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 19, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 20, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 21, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 22, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 23, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 24, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 25, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 26, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 27, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 28, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 29, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 30, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 31, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 32, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 33, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 34, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 35, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 36, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 37, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 38, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 39, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 40, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 41, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 42, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 43, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 44, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 45, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 46, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 47, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 48, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 49, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 50, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 51, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 52, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 53, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 54, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
+  { id: 55, enter_text: "", leave_text: "", flx: 300, speech_bubble: "" },
 ];
 
 const App = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+  };
+
   return (
-    <div className="w-full h-full bg-gray-500 bg-gradi bg-gradient-to-b from-indigo-500 to-gray-950 flex flex-col items-center min-h-screen ">
-      <div className=" flex items-center justify-center relative mt-8 ">
-        <h1 className=" absolute text-[94px] text-[#df781e] ">Comic Gen</h1>
-        <h1 className=" text-[98px]  font-extrabold ">Comic Gen</h1>
+    <div className="w-full h-full bg-gray-500  bg-gradient-to-b from-indigo-800 to-gray-900 flex flex-col items-center min-h-screen ">
+      <div className=" flex flex-col p-6 w-full items-center justify-center relative mt-8 ">
+        <h4 className=" text-lg mb-0 sm:text-xl lg:text-3xl text-white">
+          Your live ai comic generator
+        </h4>
+        <h1 className=" tracking-[3px] -mt-4 text-[44px] sm:text-[94px] text-white ">
+          Comic Gen
+        </h1>
+
+        <div className="flex mb-4 flex-col border-2 border-black -skew-x-6 items-center bg-white p-4">
+          <p>Turn on Edit mode to add the text and speech</p>
+          <p> add prompt to genrate images</p>
+          <p>Turn off Edit mode to view as comic.</p>
+          <p>any field left empty will not be displayed in final comic</p>
+        </div>
+
+        <div className=" flex items-center gap-4">
+          <span className=" mb-1 text-white text-xl">Edit Mode</span>
+          <button
+            className={`relative px-2 w-14 h-8 rounded-full focus:outline-none ${
+              isOn ? "bg-green-500" : "bg-gray-300"
+            }`}
+            onClick={toggleSwitch}
+          >
+            <span
+              className={`absolute inset-y-0 left-0 w-6 h-6 mx-1.5 m-auto rounded-full bg-white shadow-md transform 
+            ${isOn ? "translate-x-[22px]" : ""} 
+            transition-transform duration-300 ease-in-out`}
+            ></span>
+          </button>
+        </div>
       </div>
 
-      <div className="w-full h-full mb-12 bg-white p-12 max-w-7xl flex flex-wrap gap-4 ">
+      <div className="w-full h-full mb-12 bg-white gap-4 p-6 max-w-7xl flex flex-wrap  ">
         {cardContent.map((card) => (
           <Card
             id={card.id}
-            prompt={card.prompt}
+            prompt={""}
             flx={card.flx}
             enter={card.enter_text}
             leave={card.leave_text}
             speech={card.speech_bubble}
             bold={card.bold_text}
+            editMode={isOn}
           />
         ))}
-
-        {/* <ComicBook /> */}
       </div>
     </div>
   );
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,55 @@
 import React, { useState, useEffect } from "react";
+import "../Images/1.jpg";
 
-const Card = ({ id, prompt, flx, enter, leave, speech, bold }) => {
+const TopBottomText = ({ text, editMode, styles }) => {
+  // console.log(text);
+  const [inputText, setInputText] = useState(text);
+  return (
+    (editMode || inputText) && (
+      <input
+        type="text"
+        value={inputText}
+        onChange={(event) => {
+          setInputText(event.target.value);
+        }}
+        className={`bg-white max-w-full ${styles} px-3 pb-1.5 h-7 absolute min-w-[100px] border-black border-2 -skew-x-12  `}
+        placeholder="Enter Text..."
+        style={{ width: `${inputText.length * 8}px` }} // Adjust the width based on content length
+        disabled={!editMode} // Disable input while image is generating
+      />
+    )
+  );
+};
+
+const SpeechBubble = ({ speech, editMode, styles }) => {
+  const [inputText, setInputText] = useState(speech);
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  return (
+    (editMode || inputText) && (
+      <div className={`relative ${styles}`}>
+        <textarea
+          value={inputText}
+          onChange={handleInputChange}
+          className=" -skew-x- disabled:bg-white disabled:opacity-90 overflow-hidden border-2 -mx-4 border-black max-w-full max-h-full resize min-w-[60px] text-center min-h-[40px] p-2 rounded-t-[40px] rounded-l-[40px] focus:outline-none"
+          placeholder="Enter Text..."
+          disabled={!editMode}
+        />
+      </div>
+    )
+  );
+};
+
+const Card = ({ id, prompt, flx, enter, leave, speech, bold, editMode }) => {
   const [inputText, setInputText] = useState(prompt);
   const [topLeft, setTopLeft] = useState(enter);
   const [bottomRight, setBottomRight] = useState(leave);
   const [speechBubble, setSpeechBubble] = useState(speech);
   const [actionText, setActionText] = useState(bold);
+  const [speechBubbleRight, setSpeechBubbleRight] = useState("Right Speech");
 
   const [generatedImage, setGeneratedImage] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -49,80 +93,74 @@ const Card = ({ id, prompt, flx, enter, leave, speech, bold }) => {
     return result;
   };
 
-  useEffect(() => {
-    if (firstRender) {
-      setFirstRender(false);
-      handleImageGeneration(); // Generate image with default prompt on initial render
-    }
-  }, [firstRender]);
+  // useEffect(() => {
+  //   handleImageGeneration(); // Generate image with default prompt on initial render
+  //   // console.log('i fire once');
+  // }, []);
+
+  // console.log(id);
+  // console.log(enter);
+  // console.log(leave);
 
   return (
-    <div className="panel -skew-x-2 w-full h-[450px] overflow-hidden relative border-black border-[3px]">
-      <div className="absolute z-10 translate-y-[200px] flex w-[calc(100%-3rem)] m-6">
-        <input
-          type="text"
-          value={inputText}
-          onChange={handleInputChange}
-          className="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-white px-3 py-2.5 pr-20 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all 
-            placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 
+    <div
+      style={{ flexBasis: `${flx}px` }}
+      className={`flex-1 shadow-md m-2 -skew-x-2 h-[450px] relative border-black border-[3px]`}
+    >
+      {editMode && (
+        <div className="absolute z-10 translate-y-[320px] flex w-[calc(100%-3rem)] m-6">
+          <input
+            type="text"
+            value={inputText}
+            onChange={handleInputChange}
+            className="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-white px-2 py-2.5 pr-12 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all 
+            placeholder-shown:border shadow-sm shadow-cyan-900 border-cyan-950 placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 
              focus:border-cyan-800 focus:outline-0 
              disabled:bg-blue-gray-50"
-          placeholder="Enter Image Prompt..."
-          disabled={loading} // Disable input while image is generating
-        />
-        <button
-          className={`!absolute w-28 right-1 top-1 z-10 select-none rounded  bg-cyan-700 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-cyan-500/20 transition-all 
+            placeholder="Enter Image Prompt..."
+            disabled={loading} // Disable input while image is generating
+          />
+          <button
+            className={`!absolute  right-1 top-1 z-10 select-none rounded  bg-cyan-700 py-2 px-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-cyan-500/20 transition-all 
           ${
             loading
               ? ` bg-gray-400`
-              : `hover:shadow-lg hover:shadow-cyan-500/40 
-          focus:opacity-[0.85] focus:shadow-none 
-          active:opacity-[0.85] active:shadow-none 
-          peer-placeholder-shown:pointer-events-none peer-placeholder-shown:bg-blue-gray-500 peer-placeholder-shown:opacity-50 peer-placeholder-shown:shadow-none`
+              : `hover:shadow-lg hover:shadow-cyan-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none peer-placeholder-shown:pointer-events-none peer-placeholder-shown:bg-blue-gray-500 peer-placeholder-shown:opacity-50 peer-placeholder-shown:shadow-none`
           }`}
-          type="button"
-          data-ripple-light="true"
-          onClick={handleImageGeneration}
-          disabled={loading} // Disable button while image is generating
-        >
-          {!loading ? <>Generate</> : <>Generating</>}
-        </button>
-      </div>
-      <div className=" absolute w-full h-full">
-        <input
-          type="text"
-          value={topLeft}
-          onChange={(event) => {
-            setTopLeft(event.target.value);
-          }}
-          className={`bg-white -left-2 -top-1 py-0.5 px-3 h-7 absolute min-w-[100px] border-black border-2 -skew-x-12 `}
-          placeholder="Enter Text..."
-          style={{ width: `${topLeft.length * 8}px` }} // Adjust the width based on content length
-          // disabled={loading} // Disable input while image is generating
+            type="button"
+            data-ripple-light="true"
+            onClick={handleImageGeneration}
+            disabled={loading} // Disable button while image is generating
+          >
+            GO
+          </button>
+        </div>
+      )}
+      <div className=" absolute w-full h-full ">
+        <TopBottomText
+          editMode={editMode}
+          text={enter}
+          styles={`-left-[10px] -top-2`}
         />
-        <input
-          type="text"
-          value={bottomRight}
-          onChange={(event) => {
-            setBottomRight(event.target.value);
-          }}
-          className={`bg-white -right-2 -bottom-1 px-3 pb-1.5 h-7 absolute min-w-[100px] border-black border-2 -skew-x-12  `}
-          placeholder="Enter Text..."
-          style={{ width: `${bottomRight.length * 8}px` }} // Adjust the width based on content length
-          // disabled={loading} // Disable input while image is generating
+        <TopBottomText
+          editMode={editMode}
+          text={leave}
+          styles={`-right-[10px] -bottom-2`}
         />
-        {/* <p class="  ">{enter}</p> */}
-        {/* <p class="speech">{speech}</p> */}
+
+        <SpeechBubble editMode={editMode} speech={speech} styles={`mt-8`} />
       </div>
-      {generatedImage && (
+      {
         <div className="w-full h-full">
           <img
             className="w-full h-full object-cover overflow-hidden"
-            src={generatedImage}
+            src={
+              generatedImage ? generatedImage : require(`../Images/${id}.jpg`)
+            }
             alt="Generated"
           />
         </div>
-      )}
+      }
     </div>
   );
 };
